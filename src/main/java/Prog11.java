cf1fa70b8a36a3cedc610c2790245da02958dc7c
4721208 <130113245+4721208@users.noreply.github.com>
2023-04-07 10:50:38 +0900

    Update Prog11.java

--- src/main/java/Prog11.java
+++ src/main/java/Prog11.java
@@ -1,9 +1,14 @@
+/**
+ * クラス Prog11
+ * 
+ * @author (4721208 佐藤友麻)
+ * @version (2022/04/07)
+ */
 public class Prog11 {
 
 	public static void main(String[] args) {
-		// ここにプログラムを追加すること.
-		System.out.println(className); // この行は変更しない
+		String className = "プログラミング実習";
+		System.out.println(className); 
 	}
 
 }
-
